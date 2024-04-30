fc1819d57978ab8bf1b04e36547f8bad46534965
devvieira <kauavieira900@gmail.com>
2024-04-29 21:03:54 -0300

    feat: add components header and page

---
+++ app/_components/search.tsx
@@ -0,0 +1,17 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const Search = () => {
+  return (
+    <div className="flex gap-2">
+      <Input placeholder="Buscar resturantes..." className="border-none" />
+
+      <Button>
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
