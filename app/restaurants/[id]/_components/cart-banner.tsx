1477fb7217438a1d1ec7486d2447c412cc71fa09
devvieira <kauavieira900@gmail.com>
2024-05-03 00:14:13 -0300

    style: changed cart-banner style

--- app/restaurants/[id]/_components/cart-banner.tsx
+++ app/restaurants/[id]/_components/cart-banner.tsx
@@ -28,7 +28,7 @@ const CartBanner = ({ restaurant }: CartBannerProps) => {
   if (!restaurantHasProductsOnCart) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full border-t border-solid border-muted bg-white p-5 pt-3 shadow-md">
+    <div className="fixed bottom-0 left-0 z-50 w-full rounded-tl-2xl rounded-tr-2xl border-t border-solid border-muted bg-white p-5 pt-3 shadow-lg">
       <div className="flex items-center justify-between">
         {/* PREÇO */}
         <div className="">
